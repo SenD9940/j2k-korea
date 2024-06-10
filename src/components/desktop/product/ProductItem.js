d92cd2c10cc0584d1cfb487814ccdde24273282f
FBF <49613896+SenD9940@users.noreply.github.com>
2024-06-10 17:54:03 +0900

    레아아웃 수정
    데스크톱 레이아웃 수정

--- src/components/desktop/product/ProductItem.js
+++ src/components/desktop/product/ProductItem.js
@@ -1,57 +1,104 @@
 import styled from "styled-components";
 
-function ProductItem({image, subTitle, title}){
+function ProductItem({image, category, subTitle, title, event, discount}){
     return(
         <Container>
             <BackgroundImage src={image} alt="service1"/>
-            <Background></Background>
-            <TitleWrap>
-                <SubTitle>{subTitle}</SubTitle>
-                <Title>{title}</Title>
-            </TitleWrap>
+            <Contents>
+                <ContentsHeader>
+                    <Category>{category}</Category>
+                    <OptionWrap>
+                        {discount ? <Discount>특가</Discount> : null}
+                        {event ? <Event>EVENT</Event> : null}
+                    </OptionWrap>
+                </ContentsHeader>
+                <TitleWrap>
+                    <Title>{title}</Title>
+                    <SubTitle>{subTitle}</SubTitle>
+                </TitleWrap>
+            </Contents>
         </Container>
     )
 }
 
 const Container = styled.div`
-    position: relative;
-`
-
-const Background = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(17, 17, 17, 0.30);
-    z-index: 1;
+    flex-basis: 100%;
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    box-shadow: 0px 4px 38.1px 0px rgba(0, 0, 0, 0.15);
+    gap: 0px;
 `
 
 const BackgroundImage = styled.img`
     width: 100%;
+    height: 640px;
+    overflow: hidden;
+    object-fit:cover;
+`
+
+const Contents = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    padding: 20px;
+    flex-grow: 1;
+    background: #F8F8F8;
+`
+
+const ContentsHeader = styled.div`
+    display: flex;
+    gap: 20px;
+    align-items: center;
+`
+
+const OptionWrap = styled.div`
+    display: flex;
+    gap: 8px;
+`
+
+const Event = styled.div`
+    color: #FFF;
+    text-align: center;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: grey;
+    padding: 4px;
+`
+
+const Discount = styled.div`
+    color: #FFF;
+    text-align: center;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: #4A1B65;
+    padding: 4px;
+`
+
+const Category = styled.div`
+    color: #4A1B65;
+    font-size: 20px;
+    font-weight: 400;
 `
 
 const SubTitle = styled.div`
-    color: #FFF;
+    color: #000;
     font-size: 16px;
     font-weight: 500;
 `
 
 const Title = styled.div`
-    color: #FFF;
-    font-size: 40px;
-    font-weight: 600;
+    color: #000;
+    font-size: 24px;
+    font-weight: 400;
     white-space: pre-line;
 `
 
 const TitleWrap = styled.div`
-    position: absolute;
-    left: 50px;
-    bottom: 140px;
-    z-index: 2;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+   display: flex;
+   flex-direction: column;
+   gap: 10px;
 `
 
 export default ProductItem;
--- src/components/desktop/product/ProductList.js
+++ src/components/desktop/product/ProductList.js
@@ -3,24 +3,27 @@ import ProductItem from "./ProductItem";
 import SERVICE_1_SVG from "../../../images/service1.svg";
 import SERVICE_2_SVG from "../../../images/service2.svg";
 import SERVICE_3_SVG from "../../../images/service3.svg";
-import { Slide } from "react-awesome-reveal";
+import SOPAL_SVG from "../../../images/sopal.svg";
+import { Fade, Slide } from "react-awesome-reveal";
+import { useNavigate } from "react-router-dom";
 
 function ProductList(){
+    const navigate = useNavigate();
     return(
         <Container>
             <div className="inner">
-                <Slide >
-                    <ProductItem image={SERVICE_1_SVG} subTitle={"상품 기획 및 유통"} title={"재고 상품 재상품화\n솔루션 & 홈쇼핑 판매"}/>
-                </Slide>
-                <Slide>
-                    <ProductItem image={SERVICE_2_SVG} subTitle={"DT 솔루션"} title={"재고 상품 역경매형\nDT 솔루션"}/>
-                </Slide>
-                <Slide>
-                    <ProductItem image={SERVICE_3_SVG} subTitle={"인공지능 솔루션"} title={"학습자 성향에 따른\n온라인 학습 프로그램"}/>
-                </Slide>
-                <Slide>
-                    <ProductItem image={SERVICE_3_SVG} subTitle={"인공지능 솔루션"} title={"동작 포착형(Motion Detection)\n홈 트레이닝 프로그램"}/>
-                </Slide>
+                <ItemWrap>
+                    <ProductItem image={SOPAL_SVG} category={"FASHION"} title={"불편함 제로 기능성 남성속옷 쏘팔팬티"} subTitle={"원적외선 방사 세포의 활성화, 재생촉진으로 남성 성기능 질환 완환에 탁월"} event={true} discount={true}/>
+                    <ProductItem image={SERVICE_1_SVG} category={"SERVICE"} title={"상품 기획 및 유통"} subTitle={"재고 상품 재상품화\n솔루션 & 홈쇼핑 판매"} event={true}/>
+                </ItemWrap>
+                <ItemWrap>
+                    <ProductItem image={SERVICE_2_SVG} category={"SERVICE"} title={"DT 솔루션"} subTitle={"재고 상품 역경매형\nDT 솔루션"} discount={true}/>
+                    <ProductItem image={SERVICE_3_SVG} category={"SERVICE"} title={"인공지능 솔루션"} subTitle={"학습자 성향에 따른\n온라인 학습 프로그램"} discount={true}/>
+                </ItemWrap>
+                <ProductItem image={SERVICE_3_SVG} category={"SERVICE"} title={"인공지능 솔루션"} subTitle={"동작 포착형(Motion Detection)\n홈 트레이닝 프로그램"} event={true}/>
+                <Fade>
+                    <Button onClick={() => navigate("/contact")}>구매문의하기</Button>
+                </Fade>
             </div>
         </Container>
     )
@@ -35,5 +38,21 @@ const Container = styled.div`
         gap: 24px;
     }
 `
+
+const ItemWrap = styled.div`
+    display: flex;
+    gap: 20px;
+`
+
+const Button = styled.div`
+    background-color: #4A1B65;
+    color: white;
+    font-size: 20px;
+    padding: 12px;
+    font-weight: 600;
+    text-align: center;
+    cursor: pointer;
+`
+
 
 export default ProductList;
